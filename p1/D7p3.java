9bfdd2f3f3bb1b8789dd56e804c0030d588898de
Ayush <113271472+Ayu-aiml@users.noreply.github.com>
2024-08-14 11:23:39 +0530

    Add files via upload

---
+++ p1/D7p3.java
@@ -0,0 +1,17 @@
+package p1;
+
+public class D7p3 {
+    private String name;
+    private int roll_no;
+    
+    public D7p3(String name, int roll_no) {
+        this.name = name;
+        this.roll_no = roll_no;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + roll_no);
+    }
+    //TO RUN THE GO TO MARKS.JAVA AND RUN IT
+}
